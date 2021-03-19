16597434d265caf397868b4c647a9f23944e87fc
Imgaard <jkjerg19@student.aau.dk>
2021-03-19 13:30:23 +0100

    Lexer finish with errors
    fixME

--- LC/src/main.c
+++ LC/src/main.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
+#include "include/lexer.h"
 
 
 int main(int argc, char* argv[])
 {
-    printf("Hello World!\n");
+    //Creating the lexer
+    lexer_T* lexer = init_lexer(
+       
+        "var name = \"Magni halfdorson\";\n"
+        "print(name);"
+    );
+
+    token_T* token = (void*)0;
+
+    while ((token = lexer_get_next_token(lexer)) != (void*)0)
+    {
+        printf("TOKEN(%d, %s)\n", token -> type, token -> value);
+    }
 
     return 0;
 }
