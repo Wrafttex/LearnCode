c4450ccc06453b7ee1f76664eeedaa24a9ff4fbd
Imgaard <jkjerg19@student.aau.dk>
2021-04-09 21:30:27 +0200

    Changes to Main
    Main now asks for file path instead of hardcoding file path

--- LCTLang/src/LCTlang/Main/LCTMain.java
+++ LCTLang/src/LCTlang/Main/LCTMain.java
@@ -3,26 +3,48 @@ package LCTlang.Main;
 import LCTlang.statements.StatementVisitor;
 import LCTlang.LCTLexer;
 import LCTlang.LCTParser;
-import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import java.io.IOException;
 
 public class LCTMain {
-    public static void main(String[] args)
-    {
-        try{
-            LCTLexer lexer = new LCTLexer(new ANTLRFileStream("Tests/test.LCT"));
-            LCTParser parser = new LCTParser(new CommonTokenStream(lexer));
+    public static void main(String[] args) {
+
+        if (args.length != 1) {
+            System.err.print("Usage: file name");
+        } else {
+            String fileName = args[0];
+            LCTParser parser = getParser(fileName);
+
+            // tell ANTLR to build a parse tree
             ParseTree tree = parser.program();
-            StatementVisitor visitor = new StatementVisitor();
-            visitor.visit(tree);
-        } catch (Exception ex) {
-            if (ex instanceof RuntimeException) {
-                System.out.println(ex.getMessage());
-            } else if (ex instanceof IOException){
-                ex.printStackTrace();
-            }
+
+
+            // Create a visitor for converting the parse tree into Program/Expression object
+            StatementVisitor Visitor = new StatementVisitor();
+            Visitor.visit(tree);
         }
     }
+
+
+        private static LCTParser getParser(String filename){
+            LCTParser parser = null;
+            try {
+                CharStream input = CharStreams.fromFileName(filename);
+                LCTLexer lexer = new LCTLexer(input);
+                CommonTokenStream tokens = new CommonTokenStream(lexer);
+                parser = new LCTParser(tokens);
+            } catch (Exception ex) {
+                if (ex instanceof RuntimeException) {
+                    System.out.println(ex.getMessage());
+                } else if (ex instanceof IOException) {
+                    ex.printStackTrace();
+                }
+            }
+            return parser;
+        }
+
+
 }
